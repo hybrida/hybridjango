e939e7df97b20efc048464525bcbb4d582212257
Kia <kia.brekke@gmail.com>
2016-09-27 20:28:31 +0200

    new url

--- hybridjango/urls.py
+++ hybridjango/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='frontpage.html')),
     url(r'^kilt/', include('apps.kiltbestilling.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^strikk/', TemplateView.as_view(template_name='hybridastrikk.html'))
     url(r'^bedkom/', include('apps.bedkom.urls')),
 ]
