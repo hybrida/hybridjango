d7c67182c64ccd54468620f7f84e7a7b9ded04b0
Sirikam <ben.dik@live.no>
2016-09-27 19:17:55 +0200

    la til en bestillings side

--- apps/kiltbestilling/urls.py
+++ apps/kiltbestilling/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'bestilling', views.bestilling)
 ]
--- apps/kiltbestilling/views.py
+++ apps/kiltbestilling/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "kiltbestilling/test.html")
+
+def bestilling(request):
+    return render(request, "kiltbestilling/bestilling.html")
