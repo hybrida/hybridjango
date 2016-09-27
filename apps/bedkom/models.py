2014b9f831fa28a4e268977256da1bc25eef349b
Jonas <jonassagild@hotmail.com>
2016-09-27 22:11:59 +0200

    tabeller bedkom

--- apps/bedkom/models.py
+++ apps/bedkom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bedrift:
+    navn = None
+    ansvarlig = None
+    kontaktperson = None
+
+    def __init__(self, navn, ansvarlig, kontaktperson):
+        self.navn = navn
+        self.ansvarlig = ansvarlig
+        self.kontaktperson = kontaktperson
--- apps/bedkom/views.py
+++ apps/bedkom/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Bedrift
 
 # Create your views here.
+
+bedrifter = [
+    Bedrift("Norconsult", "Jonas", "Anna Malmø"),
+    Bedrift("Norconsult", "Jonas", "Anna Malmø"),
+    Bedrift("Norconsult", "Jonas", "Anna Malmø"),
+    Bedrift("Norconsult", "Jonas", "Anna Malmø"),
+    Bedrift("Norconsult", "Jonas", "Anna Malmø"),
+]
+
+
 def index(request):
-    return render(request, "bedkom/bedrifter.html")
+    if(request in bedrifter):
+        print(request.GET["ansvarlig"])
+    return render(request, "bedkom/bedrifter.html", {"bedrifter": bedrifter})
