751ebf1cfb9d6f9f0a2d207e4c5c74edc20be427
Jonas <jonassagild@hotmail.com>
2016-09-27 20:36:52 +0200

    Changed name

--- apps/bedkom/views.py
+++ apps/bedkom/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "bedkom/test.html")
+    return render(request, "bedkom/bedrifter.html")
