99c2fcb23fc77c0e17cc07d5ebbd7d418b8a00d9
Andreas Strand <anstra@stud.ntnu.no>
2016-10-04 20:26:10 +0200

    sidebar og statiske sider

--- apps/kiltbestilling/urls.py
+++ apps/kiltbestilling/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^info', views.index, name='info'),
+    url(r'^$', views.index, name='info'),
     url(r'shop', views.bestilling, name='shop'),
     url(r'bestilling', views.shop, name='bestilling'),
     url(r'admin', views.admin, name='admin'),
