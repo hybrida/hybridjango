6dee229955c79a5ea9b1793c6dcc2ceb8186e8c2
Jonas <jonassagild@hotmail.com>
2016-10-12 19:31:56 +0200

    changes to database

--- apps/bedkom/urls.py
+++ apps/bedkom/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^bedrift/(?P<company_id>[0-9]+)$', views.bedrift, name='bedrift'),
     url(r'^(?P<company_pk>[0-9]+)/kommenter/$', views.comment, name='comment'),
+    url(r'^bedrift/bedpress/(?P<bedpress_id>[0-9]+)$', views.bedpress, name='bedpress')
 ]
