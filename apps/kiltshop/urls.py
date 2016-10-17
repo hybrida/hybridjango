5b1c07f42d73abe6f0771c2bc676f9f1c3215f03
Simen Norderud Jensen <simennj@outlook.com>
2016-10-18 00:38:57 +0200

    seperate kiltshop names into "kilt" namespace because they are so generic

--- apps/kiltshop/urls.py
+++ apps/kiltshop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'kilt'
 urlpatterns = [
     url(r'^$', views.index, name='info'),
     url(r'shop', views.bestilling, name='shop'),
