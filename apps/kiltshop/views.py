adf38c85515bd31e86e2a94f823a3028fff32439
Andreas Strand <anstra@stud.ntnu.no>
2016-10-18 20:04:09 +0200

    made models for kiltshop and added setup for admins

--- apps/kiltshop/views.py
+++ apps/kiltshop/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, "kiltshop/info.html")
 
+def info2(request):
+    return render(request, "kiltshop/info2.html")
+
 def bestilling(request):
     return render(request, "kiltshop/bestilling.html")
 
