9ee3842ffe9ffc419075508d6fb6c082b007c92e
Andreas Strand <anstra@stud.ntnu.no>
2016-10-18 20:04:09 +0200

    made models for kiltshop and added setup for admins

--- apps/kiltshop/models.py
+++ apps/kiltshop/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-#class kilt(models.Model):
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='products', default='placeholder-event.png')
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    products = models.ManyToManyField(Product)
+    user = models.ForeignKey(User)
