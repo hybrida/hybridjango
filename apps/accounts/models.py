9c41b12dca0af150a24a4cdc3e9fdcafddbdd5f3
simen <simennj@outlook.com>
2016-10-23 21:21:27 +0200

    add participants list to event view

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,18 +1,24 @@
 import os
 
+from datetime import timedelta
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+
+
+def get_graduation_year(grade):
+    return (timezone.now() + timedelta(weeks=-26)).year + int(grade)
 
 
 def five_years():
     return timezone.now().year + 5
 
 
-class Hybrid(models.Model):
-    def user_folder(instance, filename):
-        return os.path.join('users', instance.user.username, filename)
+def user_folder(instance, filename):
+    return os.path.join('users', instance.user.username, filename)
 
+
+class Hybrid(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     middle_name = models.CharField(max_length=50, blank=True)
     graduation_year = models.IntegerField(default=five_years)
@@ -25,6 +31,9 @@ class Hybrid(models.Model):
         else:
             first_name = self.user.first_name
         return first_name + ' ' + self.user.last_name
+
+    def get_grade(self):
+        return self.graduation_year - (timezone.now() - timedelta(weeks=26)).year
 
     def __str__(self):
         return self.user.username
