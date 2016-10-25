d0b083694fd9484ce89a9a8104ec3e18ee1a82e6
Andreas Strand <anstra@stud.ntnu.no>
2016-10-25 21:12:19 +0200

    Changes to admin interface.  added "type" in product model.  small changes to "clean" the code (English/PEP8)

--- apps/kiltshop/views.py
+++ apps/kiltshop/views.py
@@ -8,18 +8,20 @@ def index(request):
 def info2(request):
     return render(request, "kiltshop/info2.html")
 
-def bestilling(request):
-    user = request.user
-    order = Order.objects.filter(user=user)
-    products = Product.objects.filter(order=order)
-    return render(request, "kiltshop/bestilling.html", {'products': products})
+
+def order(request):
+    if not request.user.is_authenticated():
+        return render(request, 'accounts/login.html')
+    else:
+        user = request.user
+        order = Order.objects.filter(user=user)
+        products = Product.objects.filter(order=order)
+        return render(request, "kiltshop/bestilling.html", {'products': products})
+
 
 def shop(request):
     products = Product.objects.all()
     return render(request, "kiltshop/shop.html", {'products': products})
-
-
-
 
 
 def admin(request):
