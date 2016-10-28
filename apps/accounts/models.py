959ea85b07be93914efffc77040d016691a8e703
simen <simennj@outlook.com>
2016-10-28 17:51:49 +0200

    add import script for users (csv exported from old database)

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -21,9 +21,12 @@ def user_folder(instance, filename):
 class Hybrid(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     middle_name = models.CharField(max_length=50, blank=True)
+    member = models.BooleanField(default=False)
     graduation_year = models.IntegerField(default=five_years)
     image = models.ImageField(upload_to=user_folder, default='placeholder-profile.jpg')
-    gender = models.CharField(max_length=20, blank=True)
+    gender = models.CharField(max_length=1, blank=True)
+    specialization = models.CharField(max_length=20, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def get_full_name(self):
         if self.middle_name:
