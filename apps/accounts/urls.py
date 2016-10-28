c524cf051f81253f071e855cfdfbec4baeab8b25
simen <simennj@outlook.com>
2016-10-28 22:54:34 +0200

    add password reset by email

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^login$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout$', logout, name='logout'),
     url(r'(?P<pk>[0-9]+)$', views.Profile.as_view(), name='profile'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.complete_registration, name='complete_registration'),
 ]
