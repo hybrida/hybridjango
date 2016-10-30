9128e185db95f37d7d62b7a9a52499a4a42c4364
Jonas <jonassagild@hotmail.com>
2016-10-30 17:31:12 +0100

    added function for changing and adding companies

--- apps/bedkom/urls.py
+++ apps/bedkom/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from apps.bedkom import views
+from apps.bedkom.views import BedriftEndre, BedriftLag
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^bedrift/(?P<company_id>[0-9]+)$', views.bedrift, name='bedrift'),
-    url(r'^(?P<company_pk>[0-9]+)/kommenter/$', views.comment, name='comment'),
-    url(r'^bedrift/bedpress/(?P<bedpress_id>[0-9]+)$', views.bedpress, name='bedpress')
+    url(r'^bedrift/(?P<pk>[0-9]+)$', views.bedrift, name='bedrift'),
+    url(r'^(?P<pk>[0-9]+)/kommenter/$', views.comment, name='comment'),
+    url(r'^bedrift/bedpress/(?P<pk>[0-9]+)$', views.bedpress, name='bedpress'),
+    url(r'^bedrift/(?P<pk>[0-9]+)/endre$', BedriftEndre.as_view(), name='endre_bedrift'),
+    url(r'^nybedrift/', BedriftLag.as_view(), name='lag_bedrift'),
+
 ]
--- apps/bedkom/views.py
+++ apps/bedkom/views.py
@@ -1,6 +1,13 @@
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import DateField
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
+
+from apps.bedkom.forms import CompanyForm
+from apps.events.models import Event
 from .models import Company, CompanyComment, EarlierBedpresses
 
 
@@ -11,19 +18,39 @@ def index(request):
     return render(request, "bedkom/bedrifter.html", {"companies": companies})
 
 
-def bedrift(request, company_id):
+def bedrift(request, pk):
     companies = Company.objects.all()
-    bedpresses = EarlierBedpresses.objects.filter(company_id=company_id)
-    return render(request, "bedkom/bedrift.html", {"company": companies.get(pk=company_id), "bedpresses": bedpresses})
+    bedpresses = EarlierBedpresses.objects.filter(company_id=pk)
+    return render(request, "bedkom/bedrift.html", {"company": companies.get(pk=pk), "bedpresses": bedpresses})
 
 
-def comment(request, company_pk):
+def comment(request, pk):
     companies = Company.objects.all()
     for company in companies:
         company.last_comment = company.companycomment_set.order_by('timestamp').last()
     return render(request, "bedkom/bedrifter.html", {"companies": companies})
 
-def bedpress(request, bedpress_id):
+
+def bedpress(request, pk):
     bedpresses = EarlierBedpresses.objects.all()
-    bedpress = bedpresses.get(pk=bedpress_id)
+    bedpress = bedpresses.get(pk=pk)
     return render(request, "bedkom/bedpress.html", {"bedpress": bedpress})
+
+
+class BedriftEndre(LoginRequiredMixin, generic.UpdateView):
+    model = Company
+    form_class = CompanyForm
+
+    def get_success_url(self):
+        return reverse('bedrift', kwargs={'pk': self.object.pk})
+
+
+class BedriftLag(LoginRequiredMixin, generic.CreateView):
+    model = Company
+    form_class = CompanyForm
+
+    def get_success_url(self):
+        return reverse('bedrift', kwargs={'pk': self.object.pk})
+
+
+
