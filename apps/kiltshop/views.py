3b85881b3392057dc366023f537d64064714b42a
Andreas Strand <anstra@stud.ntnu.no>
2016-10-30 22:18:57 +0100

    added delete feature to order page small changes to shop interface

--- apps/kiltshop/views.py
+++ apps/kiltshop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Product, Order
 
 
@@ -13,24 +14,35 @@ def order(request):
     if not request.user.is_authenticated():
         return render(request, 'registration/login.html')
     else:
+        print(request.POST)
         user = request.user
-        return render(request, "kiltshop/bestilling.html", {'products':Product.objects.filter(order=Order.objects.filter(user=user))})
+        if request.method == 'POST':
+            delete = request.POST.get('delete')
+            order = Order.objects.filter(user=user).first()
+            order.products.remove(delete)
+            order.save()
+
+        return render(request,"kiltshop/bestilling.html", {'products': Product.objects.filter(order=Order.objects.filter(user=user))})
 
 
 def shop(request):
     user = request.user
     if request.method == 'POST':
         products = request.POST.getlist('product', None)
-        if products is not None:
-            if Order.objects.filter(user=user).exists():
-                order = Order.objects.filter(user=user).first()
-                order.products.add(*products)
-                print(order)
-                order.save()
-            else:
-                order = Order.objects.create(user=user)
-                order.products.add(*products)
-                order.save()
+        if len(products) > 0:
+            if products is not None:
+                if Order.objects.filter(user=user).exists():
+                    order = Order.objects.filter(user=user).first()
+                    order.products.add(*products)
+                    order.save()
+                    return HttpResponseRedirect("/kilt/bestilling")
+                else:
+                    order = Order.objects.create(user=user)
+                    order.products.add(*products)
+                    order.save()
+                    return HttpResponseRedirect("/kilt/bestilling")
+
+
     return render(request, "kiltshop/shop.html", {"products": Product.objects.all(), "kilts": Product.objects.filter(type="k")})
 
 
