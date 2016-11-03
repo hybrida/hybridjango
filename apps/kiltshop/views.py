dd8ac2d71ea4e6a130ae5e7d1e484ae0533c3a8e
Strand <anstra@stud.ntnu.no>
2016-11-03 22:22:00 +0100

    user can now comment in shop when placing order, and see it under order status

--- apps/kiltshop/views.py
+++ apps/kiltshop/views.py
@@ -22,7 +22,8 @@ def order(request):
 
         return render(request,"kiltshop/bestilling.html",
             {'products': Product.objects.filter(order=Order.objects.filter(user=request.user)),
-             'productInfo': ProductInfo.objects.filter(order=Order.objects.filter(user=request.user).first())}
+             'productInfo': ProductInfo.objects.filter(order=Order.objects.filter(user=request.user).first()),
+             'order': user_order}
         )
 
 
@@ -33,58 +34,61 @@ def shop(request):
     if request.method == 'POST':
         products = request.POST.getlist('product', None)
         if not len(products) > 0:
-            messages.warning(request, 'Ingen produkter er valgt')
+            messages.warning(request, 'Ingen produkter er valgt.')
         else:
             new_kilt = False
             has_kilt = False
             new_sporra = False
             has_sporra = False
-            has_kiltID = 0
-            has_sporraID = 0
-            kilt_numb= 0
-            sporra_numb= 0
+            has_kilt_id = 0
+            has_sporra_id = 0
+            kilt_number = 0
+            sporra_number = 0
             for product in products:
                 if Product.objects.get(pk=product).type == 'K':
-                    kilt_numb+=1
-                    new_kilt=True
+                    kilt_number += 1
+                    new_kilt = True
                 if Product.objects.get(pk=product).type == 'S':
-                    sporra_numb+=1
-                    new_sporra=True
+                    sporra_number += 1
+                    new_sporra = True
 
-            if kilt_numb > 1:
-                messages.warning(request, 'Du har valgt mer enn en kilt!')
-            elif sporra_numb > 1:
-                messages.warning(request, 'Du har valgt mer enn en sporra')
+            if kilt_number > 1:
+                messages.warning(request, 'Du har valgt mer enn en kilt.')
+            elif sporra_number > 1:
+                messages.warning(request, 'Du har valgt mer enn en sporra.')
             else:
                 if products is not None:
                     if Order.objects.filter(user=user).exists():
-                        order = Order.objects.filter(user=user).first()
-                        for product in order.products.all():
+                        order_list = Order.objects.filter(user=user).first()
+                        for product in order_list.products.all():
                             print("count")
                             if product.type == 'K':
-                                has_kiltID=product.pk
-                                has_kilt=True
+                                has_kilt_id = product.pk
+                                has_kilt = True
                             if product.type == 'S':
                                 has_sporra=True
-                                has_sporraID=product.pk
-
+                                has_sporra_id=product.pk
 
                         if has_kilt and new_kilt:
-                            order.products.remove(has_kiltID)
+                            order_list.products.remove(has_kilt_id)
 
                         if has_sporra and new_sporra:
-                            order.products.remove(has_sporraID)
+                            order_list.products.remove(has_sporra_id)
 
+                        comment = request.POST.get('comment', None)
+                        order_list = Order.objects.filter(user=user).first()
+                        if len(comment) > 0:
+                            order_list.comment = comment
 
-
-                        order = Order.objects.filter(user=user).first()
-                        order.products.add(*products)
-                        order.save()
+                        order_list.products.add(*products)
+                        order_list.save()
                         return HttpResponseRedirect("/kilt/bestilling")
                     else:
-                        order = Order.objects.create(user=user)
-                        order.products.add(*products) # ignore this
-                        order.save()
+                        order_list = Order.objects.create(user=user)
+                        comment = request.POST.get('comment', None)
+                        order_list.comment = comment
+                        order_list.products.add(*products) # ignore this
+                        order_list.save()
                         return HttpResponseRedirect("/kilt/bestilling")
 
     return render(request, "kiltshop/shop.html", {"products": Product.objects.all(), "kilts": Product.objects.filter(type="k")})
