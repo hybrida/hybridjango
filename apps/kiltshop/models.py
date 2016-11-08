48e960a8fcbad1126e0e24dcc4127fae0a4125df
Andreas Strand <anstra@stud.ntnu.no>
2016-11-08 21:27:33 +0100

    You can now add size and numbers to your order. Kiltshop Complete!

--- apps/kiltshop/models.py
+++ apps/kiltshop/models.py
@@ -45,5 +45,8 @@ class OrderInfo(models.Model):
 class ProductInfo(models.Model):
     product = models.ForeignKey(Product)
     order = models.ForeignKey(Order)
-    number = models.IntegerField(default=1,)
-    size = models.CharField(max_length=64, blank=True)
+    number = models.IntegerField(default=1)
+    size = models.CharField(max_length=64, null=True)
+
+    class Meta():
+        auto_created = True
