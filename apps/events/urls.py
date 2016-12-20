3979335556973f61e9936a4c7f2a18bd6db824db
simen <simennj@outlook.com>
2016-12-20 13:40:37 +0100

    use frontpage as events page

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from .views import *
 
 urlpatterns = [
-    url(r'^$', EventList.as_view(), name='event_list'),
+    url(r'^$', RedirectView.as_view(pattern_name='home', permanent=False), name='event_list'),
     url(r'^(?P<pk>[0-9]+)$', EventView.as_view(), name='event'),
     url(r'^ny$', EventCreate.as_view(), name='new_event'),
     url(r'^(?P<pk>[0-9]+)/endre$', EventEdit.as_view(), name='edit_event'),
