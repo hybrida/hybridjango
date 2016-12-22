a7403347f24f806227eb11e1f8ef77328bea792e
simen <simennj@outlook.com>
2016-12-22 22:55:04 +0100

    rework about pages sidebar

--- apps/staticpages/views.py
+++ apps/staticpages/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from collections import OrderedDict
 
-# Create your views here.
+from django.urls import resolve
+from django.views.generic.base import TemplateResponseMixin, ContextMixin, View
+
+pages = [
+    ('about', "Om Hybrida"),
+    ('contact_us', "Kontakt Oss"),
+    ('committees', "Komitéer"),
+    ('griff_orden', "Griffens Orden"),
+    ('statutter', "Statutter"),
+    ('lyrics', "Sangtekster"),
+]
+
+class AboutView(TemplateResponseMixin, ContextMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        active_page = resolve(request.path_info).url_name
+        before_pages = []
+        after_pages = []
+        page_found = False
+        for page in pages:
+            if page_found:
+                after_pages.append(page)
+            else:
+                before_pages.append(page)
+                if page[0] == active_page:
+                    page_found = True
+
+        context['before_pages'] = before_pages
+        context['after_pages'] = after_pages
+        return self.render_to_response(context)
