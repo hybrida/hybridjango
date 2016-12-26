b32cf2c848d809aabc29035af72f418ba66977da
Andreas Strand <anstra@stud.ntnu.no>
2016-12-27 00:11:26 +0100

    Made it so each job has its own page. All jobs will be listed in the announcements page.

--- apps/jobannouncements/views.py
+++ apps/jobannouncements/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Job
+
 
 def index(request):
-    return render(request, "jobannoucements/announcements.html")
+    return render(request, "jobannoucements/announcements.html",
+                  {'jobs': Job.objects.all()}
+                  )
+
+class JobView(generic.DetailView):
+    model = Job
+    template_name = 'jobannoucements/job.html'
