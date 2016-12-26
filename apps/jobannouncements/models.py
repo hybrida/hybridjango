b91d19638cc353327603e91f93888fb518130793
Andreas Strand <anstra@stud.ntnu.no>
2016-12-27 00:11:26 +0100

    Made it so each job has its own page. All jobs will be listed in the announcements page.

--- apps/jobannouncements/models.py
+++ apps/jobannouncements/models.py
@@ -7,4 +7,7 @@ class Job(models.Model):
     deadline = models.DateTimeField(null=True, blank=True)
     descripion = models.TextField()
     priority = models.IntegerField(default=0)
-    logo = models.ImageField(upload_to='events', default='placeholder-event.png')
+    logo = models.ImageField(upload_to='companies', default='placeholder-event.png')
+
+    def __str__(self):
+        return str(self.title)
--- apps/jobannouncements/urls.py
+++ apps/jobannouncements/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from .views import JobView
 
 from . import views
 
 app_name = 'kilt'
 urlpatterns = [
     url(r'^$', views.index, name='info'),
+    url(r'^(?P<pk>[0-9]+)$', JobView.as_view(), name='job'),
 ]
