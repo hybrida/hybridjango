00f0a93b4b442150754346b2ed3ba699ca836cf5
Andreas Strand <anstra@stud.ntnu.no>
2016-12-29 11:47:39 +0100

    Added and started on functionally for the view, create new  and admin sites for jobannouncements.

--- apps/jobannouncements/views.py
+++ apps/jobannouncements/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Job
-
+from .forms import JobForm
+from apps.bedkom.models import Company
+from django.utils import timezone
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, "jobannoucements/announcements.html",
@@ -11,3 +14,25 @@ def index(request):
 class JobView(generic.DetailView):
     model = Job
     template_name = 'jobannoucements/job.html'
+
+
+def CreateJob(request):
+    if request.method == "POST":
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.author = request.user
+            job.timestamp = timezone.now()
+            job.save()
+            return redirect('/stillingsutlysninger/', pk=job.pk)
+    else:
+        form = JobForm()
+    return render(request, "jobannoucements/job_form.html",
+                  {'companies': Company.objects.all(),'form':form }
+                  )
+
+def JobAdmin(request):
+    return render(request, "jobannoucements/job_admin.html",
+                  {'jobs': Job.objects.all(), }
+                  )
+
