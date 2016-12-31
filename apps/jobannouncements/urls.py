b2df4eacd51561c8f7f1d37253226ebb64fab1b3
Andreas Strand <anstra@stud.ntnu.no>
2016-12-31 19:00:43 +0100

    redirecting now works as intended

--- apps/jobannouncements/urls.py
+++ apps/jobannouncements/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
 from .views import *
 
-app_name = 'kilt'
+app_name = 'jobs'
 urlpatterns = [
     url(r'^$', index, name='info'),
     url(r'^alle$', job_all, name='job_all'),
     url(r'^(?P<pk>[0-9]+)$', job_detail, name='job_detail'),
     url(r'^(?P<pk>[0-9]+)/endre/$', job_edit, name='job_edit'),
     url(r'ny', new_job, name='new'),
-
     url(r'admin', JobAdmin, name='admin'),
 ]
