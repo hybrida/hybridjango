22e4b0d15e00eb86edb780a37e97a811a66f7fc5
Andreas Strand <anstra@stud.ntnu.no>
2017-01-02 00:05:49 +0100

    Made seperate pages for active and finished jobannouncements.

--- apps/jobannouncements/views.py
+++ apps/jobannouncements/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.views import generic
 from .models import Job
 from .forms import JobForm
+import datetime
 from django.utils import timezone
 from django.shortcuts import redirect
 
 
 def index(request):
+    now = datetime.datetime.now()
+    status = "Aktive"
     return render(request, "jobannoucements/announcements.html",
-                  {'jobs': Job.objects.filter()})
+                  {'status':status, 'jobs': Job.objects.filter(deadline__gte=now).order_by('deadline')})
 
 
-def job_all(request):
+def job_previous(request):
+    now = datetime.datetime.now()
+    status = "Tidligere"
     return render(request, "jobannoucements/announcements.html",
-                  {'jobs': Job.objects.all()}
-                  )
-
+                  {'status':status,'jobs': Job.objects.filter(deadline__lte=now).order_by('deadline')})
 
 def job_detail(request, pk):
     job = get_object_or_404(Job, pk=pk)
@@ -25,6 +27,7 @@ def job_detail(request, pk):
 
 @login_required
 def new_job(request):
+    action = 'Lag ny'
     if request.method == "POST":
         form = JobForm(request.POST)
         if form.is_valid():
@@ -35,11 +38,12 @@ def new_job(request):
             return redirect('jobs:job_detail', pk=job.pk)
 
     form = JobForm(request.POST)
-    return render(request, "jobannoucements/job_form.html", {'form':form })
+    return render(request, "jobannoucements/job_form.html", {'action':action,'form':form })
 
 
 @login_required
 def job_edit(request, pk):
+    action = "Rediger"
     job = get_object_or_404(Job, pk=pk)
     if request.method == "POST":
         form = JobForm(request.POST, instance=job)
@@ -51,7 +55,7 @@ def job_edit(request, pk):
             return redirect('jobs:job_detail', pk=job.pk)
     else:
         form = JobForm(instance=job)
-    return render(request, "jobannoucements/job_form.html", {'form':form })
+    return render(request, "jobannoucements/job_form.html", {'action':action,'form':form })
 
 
 def JobAdmin(request):
