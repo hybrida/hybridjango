302cd020816c04e614282362f5698a0c58bf734b
Andreas Strand <anstra@stud.ntnu.no>
2017-01-02 00:05:49 +0100

    Made seperate pages for active and finished jobannouncements.

--- apps/jobannouncements/urls.py
+++ apps/jobannouncements/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'jobs'
 urlpatterns = [
     url(r'^$', index, name='info'),
-    url(r'^alle$', job_all, name='job_all'),
+    url(r'^tidligere$', job_previous, name='job_previous'),
     url(r'^(?P<pk>[0-9]+)$', job_detail, name='job_detail'),
     url(r'^(?P<pk>[0-9]+)/endre/$', job_edit, name='job_edit'),
     url(r'ny', new_job, name='new'),
