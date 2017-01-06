999c6b05a0fead311b7ca9a875aaedd29d8409f4
Andreas Strand <anstra@stud.ntnu.no>
2017-01-06 03:01:55 +0100

    Kiltshop: Another big admin update Jobannouncements: Adding navigation sidebar to jobpages

--- apps/kiltshop/forms.py
+++ apps/kiltshop/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import Product, Order
+from .models import Product, Order, OrderInfo
 
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['name', 'type', 'image', 'link']
+        fields = ['name', 'type', 'image', 'link','sizes']
 
 
 class OrderForm(forms.ModelForm):
@@ -14,3 +14,9 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['products', 'user']
+
+class OrderInfoForm(forms.ModelForm):
+
+    class Meta:
+        model = OrderInfo
+        fields = ['orders','startTime','endTime']
