ea72203ee5961981926354d725279157ab4cc7e5
simen <simennj@outlook.com>
2017-01-23 21:16:43 +0100

    quick, ugly and temporary placement of TP events in bedpress list

--- apps/staticpages/views.py
+++ apps/staticpages/views.py
@@ -1,8 +1,10 @@
 import os
+from itertools import chain
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import resolve
+from django.utils.datetime_safe import datetime
 from django.views.generic.base import TemplateResponseMixin, ContextMixin, View
 from django.utils import timezone
 
@@ -19,13 +21,26 @@ class FrontPage(EventList):
     queryset = EventList.queryset
 
     def get_context_data(self, **kwargs):
+
+        tz = timezone.get_current_timezone()
+        temporary_quickfix_for_tp_events = [
+            Event(pk=-1, title='Bedriftspresentasjon med DNV GL', event_start=tz.localize(datetime(2017, 1, 25, 17, 30, 0)), text='431'),
+            Event(pk=-1, title='Karrieremesse med Framtidsfylket', event_start=tz.localize(datetime(2017, 1, 26, 17)), text='407'),
+            Event(pk=-1, title='Prospective', event_start=tz.localize(datetime(2017, 2, 9)), text='440'),
+            Event(pk=-1, title='DNB Digital Challenge', event_start=tz.localize(datetime(2017, 2, 10)), text='430'),
+            Event(pk=-1, title='Karrieremesse med Haugesundsregionen', event_start=tz.localize(datetime(2017, 2, 16, 18)), text='406'),
+            Event(pk=-1, title='DNV GL Opportunity Day', event_start=tz.localize(datetime(2017, 3, 2, 10, 15)), text='432'),
+            Event(pk=-1, title='Teknologiporten FOKUS: InnoVention', event_start=tz.localize(datetime(2017, 3, 16, 17)), text='422'),
+        ]
+
         context = super(EventList, self).get_context_data(**kwargs)
         context['event_list_chronological'] = Event.objects.filter(
             event_start__gte=timezone.now(), bedpress__isnull=True
         ).order_by('event_start')[:5]
-        context['bedpress_list_chronological'] = Event.objects.filter(
-            event_start__gte=timezone.now(), bedpress__isnull=False
-        ).order_by('event_start')[:5]
+        context['bedpress_list_chronological'] = sorted(chain(
+            Event.objects.filter(event_start__gte=timezone.now(), bedpress__isnull=False).order_by('event_start')[:5],
+            temporary_quickfix_for_tp_events
+        ), key=lambda event: event.event_start)
         context['job_list'] = Job.objects.filter(deadline__gte=timezone.now()).order_by('-deadline').filter(priority=True).reverse()
         context['job_sidebar'] = Job.objects.filter(deadline__gte=timezone.now())
         return context
