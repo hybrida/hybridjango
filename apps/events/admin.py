0ef773d734c874090df0db4f20f9a3003b57dd8f
Andreas Strand <anstra@stud.ntnu.no>
2017-01-27 14:43:28 +0100

    change to participant adjustment in admin page for event

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Event
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    filter_horizontal = ('participants',)
+
+admin.site.register(Event, EventAdmin)
