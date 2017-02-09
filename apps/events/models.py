25c31d0f18a5c617274dae8904465f8407819edb
simen <simennj@outlook.com>
2017-02-09 01:24:31 +0100

    add some requested features to events: waiting list, grade and gender restrictions, nonpublic events, instructions for event creation, and various smaller changes. Fixes #15

--- apps/events/models.py
+++ apps/events/models.py
@@ -7,21 +7,26 @@ from django.utils import timezone
 class Event(models.Model):
     title = models.CharField(max_length=150)
     ingress = models.CharField(max_length=350, blank=True, default='')
-    text = models.TextField()
+    text = models.TextField(blank=True)
     author = models.ForeignKey(Hybrid, related_name='authored')
     timestamp = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='events', default='placeholder-event.png')
-    participants = models.ManyToManyField(Hybrid, related_name='participating', blank=True)
-    max_participants = models.IntegerField(default=0)
-    price = models.PositiveIntegerField(default=0)
-    location = models.CharField(max_length=50, blank=True)
-    signup_start = models.DateTimeField(null=True, blank=True)
-    signup_end = models.DateTimeField(null=True, blank=True)
+    image = models.ImageField(upload_to='events', blank=True)
     event_start = models.DateTimeField(null=True, blank=True)
     event_end = models.DateTimeField(null=True, blank=True)
     weight = models.IntegerField(default=0)
     hidden = models.BooleanField(default=False)
     news = models.BooleanField(default=True)
+    public = models.BooleanField(default=True)
+
+    participants = models.ManyToManyField(Hybrid, related_name='participating', blank=True)
+    waiting_list = models.ManyToManyField(Hybrid, related_name='waiting', blank=True, )
+    max_participants = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    location = models.CharField(max_length=50, blank=True)
+    signup_start = models.DateTimeField(null=True, blank=True)
+    signup_end = models.DateTimeField(null=True, blank=True)
+    genders = models.CharField(max_length=3, default='MFU')
+    grades = models.CharField(max_length=5, default='12345')
 
     def get_absolute_url(self):
         if(self.pk < 0): # TODO: replace this
@@ -33,6 +38,21 @@ class Event(models.Model):
 
     def signup_closed(self):
         return self.signup_start and self.signup_end and timezone.now() > self.signup_end
+
+    def invited(self, user):
+        if self.signup_open:
+            return user.gender in self.genders and str(user.get_grade()) in self.grades
+
+    def get_first_waiting(self):
+        return self.waiting_list.order_by('id').first()
+
+    def can_join(self, user):
+        if self.waiting_list.count():
+            print(user)
+            print(self.waiting_list.order_by('id').first().username)
+            return user.username == self.get_first_waiting()
+        return self.signup_open() and self.participants.count() < self.max_participants and self.invited(user)
+
 
     def __str__(self):
         return '{}: {}'.format(self.timestamp.date(), self.title)
