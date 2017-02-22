899b7b17789b0a8302eb4eeede886ea6615eec76
kia <kia.brekke@gmail.com>
2017-02-22 17:58:21 +0100

    strikk in inlogged navbar

--- apps/staticpages/urls.py
+++ apps/staticpages/urls.py
@@ -5,7 +5,7 @@ from apps.staticpages.views import AboutView, updatek, search, RingenView
 
 urlpatterns = [
     url(r'^s[oø]k/$', search, name='search'),
-    url(r'^strikk/$', TemplateView.as_view(template_name='staticpages/hybridastrikk.html')),
+    url(r'^strikk/$', TemplateView.as_view(template_name='staticpages/hybridastrikk.html'), name="strikk"),
     url(r'^statutter/$', AboutView.as_view(template_name='staticpages/statutter.html'), name='statutter'),
     url(r'^griffens_orden/$', AboutView.as_view(template_name='staticpages/griffens_orden.html'), name='griff_orden'),
     url(r'^om_hybrida/$', AboutView.as_view(template_name='staticpages/about.html'), name='about'),
