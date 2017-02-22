c61b68f9740188770b04c9c80dae47f58c6aea16
Sirikam <ben.dik@live.no>
2017-02-22 19:27:56 +0100

    adds a ridder and a honarary member class in models for the database

--- apps/griffensorden/admin.py
+++ apps/griffensorden/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ridder, Honary_member
+
+
+
+
 
 # Register your models here.
+admin.site.register(Ridder)
+admin.site.register(Honary_member)
--- apps/griffensorden/models.py
+++ apps/griffensorden/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.registration.models import Hybrid
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Ridder(models.Model):
+    name = models.CharField(max_length=200)
+    finished = models.CharField(max_length=4)
+    awarded = models.CharField(max_length=4)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Honary_member(models.Model):
+    name = models.CharField(max_length=200)
+    awarded = models.CharField(max_length=4)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
