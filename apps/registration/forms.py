9169ba794e28239c9dd64057e2900d87f26c78ae
simen <simennj@outlook.com>
2017-03-02 20:08:04 +0100

    redo profile form

--- apps/registration/forms.py
+++ apps/registration/forms.py
@@ -1,21 +1,19 @@
 from django import forms
+
 from .models import Hybrid
 
 
 class HybridForm(forms.ModelForm):
     class Meta:
         model = Hybrid
-        exclude = (
-            'id',
-            'password',
-            'last_login',
-            'is_superuser',
-            'username',
-            'is_staff',
-            'is_active',
-            'date_joined',
-            'member',
+        fields = (
+            'first_name',
+            'middle_name',
+            'last_name',
+            'title',
+            'email',
+            'image',
             'graduation_year',
             'specialization',
-            'gender'
+            'gender',
         )
