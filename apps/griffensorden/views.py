eed100bd79f11e9b50c5783cb2a80eb5d896e78b
Sirikam <ben.dik@live.no>
2017-03-15 19:17:42 +0100

    rewrites teh griffensorden page so that it loads all its data from the database

--- apps/griffensorden/views.py
+++ apps/griffensorden/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Ridder
 
 # Create your views here.
 
 def index(request):
-    return render(request, "griffensorden/griffens_orden.html")
+    context ={'Ridder': Ridder.objects.all()}
+    print(context)
+    print("asdf")
+    return render(request, "griffensorden/griffens_orden.html",
+                  context=context
+                  )
