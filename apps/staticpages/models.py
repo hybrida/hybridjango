b4f8b7505c99a8be38f55d0daa8976fdbbe31d86
Andreas Strand <anstra@stud.ntnu.no>
2017-03-21 20:23:55 +0100

    small changes to board report page

--- apps/staticpages/models.py
+++ apps/staticpages/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class BoardReport(models.Model):
-    report = models.FileField(upload_to='pdf/møtereferat')
+    report = models.FileField(upload_to='pdf/referat')
     date = models.DateField(default=timezone.now)
+    description = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return "Referat: "+str(self.date)
