0eba085344fc71102fd633fbd7cea21111c0027f
Ivar Krabol <dev@ivar.hk>
2017-03-28 22:26:17 +0200

    rfid appearances mvp

---
+++ apps/rfid/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from .views import *
+
+app_name = 'rfid'
+urlpatterns = [
+    url(r'^(?P<pk>[0-9]+)$', AppearancesView.as_view(), name='rfid'),
+    url(r'^(?P<pk>[0-9]+)/add$', add, name='add'),
+]
