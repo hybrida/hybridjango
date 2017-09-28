e2415471d71603015a3719c4583fe742cf65cad1
Strand <anstra@stud.ntnu.no>
2017-09-28 15:24:34 +0200

    Making it so an event only can have 1 RFID pointer.

--- apps/rfid/models.py
+++ apps/rfid/models.py
@@ -6,7 +6,7 @@ from apps.registration.models import Hybrid
 
 class Appearances(models.Model):
     users = models.ManyToManyField(Hybrid, blank=True)
-    event = models.ForeignKey(Event, null=True, blank=True, on_delete=models.DO_NOTHING)
+    event = models.ForeignKey(Event, null=True, blank=True, on_delete=models.DO_NOTHING, unique=True)
 
     def add_appearance(self, user):
         self.users.add(user)
