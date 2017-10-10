dbd2e8e9ec7aaa6605f86cd41383f6e3558a2bff
Simen Norderud jensen <simennj@outlook.com>
2017-10-10 20:15:30 +0200

    add management command for populating TP events

---
+++ apps/events/management/commands/update_tp_events.py
@@ -0,0 +1,31 @@
+import json
+
+import requests
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from apps.events.models import TPEvent
+
+
+class Command(BaseCommand):
+    help = ''
+
+    def handle(self, *args, **options):
+        event_list = requests.get(
+            'https://teknologiporten.no/intern/event_list'
+        )
+        relevant_events = [
+            {
+                'tp_id': key,
+                'title': '{} - {}'.format(value['company'], value['title']),
+                'event_start': value['time']
+            }
+            for key, value in json.loads(event_list.content).items()
+            if '17' in value['invited_programs'].keys()
+               or not value['invited_programs']
+        ]
+        with transaction.atomic():
+            for event in relevant_events:
+                TPEvent.objects.update_or_create(
+                    tp_id=event['tp_id'], defaults=event
+                )
