483e3a57573a564965b817143e496700b69f1ffd
sindreeo <sindreeo@stud.ntnu.no>
2017-10-17 19:35:53 +0200

    Changes upload folder on the badgemodel

--- apps/achievements/models.py
+++ apps/achievements/models.py
@@ -6,7 +6,10 @@ import os
 class Badge(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    badge_image = models.FileField(upload_to='uploads/badges/')
-    badge_placeholder = models.FileField(upload_to='uploads/badges')
+    badge_image = models.FileField(upload_to='badges/')
+    badge_placeholder = models.FileField(upload_to='badges/')
     scorepoints = models.IntegerField()
     user = models.ManyToManyField(Hybrid, default=None)
+
+    def __str__(self):
+        return self.name
