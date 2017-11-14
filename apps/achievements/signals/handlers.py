91826a5c7507dd6857e472bfe04e677327a8f380
Sirikam <ben.dik@live.no>
2017-11-15 00:53:44 +0100

    adds the working membership badge to the project, it will award a membership medal to anyone whos profile gets updated, and the membership status change

--- apps/achievements/signals/handlers.py
+++ apps/achievements/signals/handlers.py
@@ -16,14 +16,29 @@ griff_badge = django.dispatch.Signal()
 def my_callback(sender, **kwargs):
     print("Request finished!")
 
+#functions that award specific badges to members based on certain criterias, like membership, if they're ridders and so forth
+#===========================================================================================================================#
 #function that awards a griffens orden badge to everyone that gets added as a Ridder to the site
 @receiver(post_save, sender=Ridder)
 def GriffBadge(sender=Ridder, **kwargs):
     inst_obj = kwargs['instance']
-    badge= Badge.objects.get(name="Griffens Orden")
+    badge = Badge.objects.get(name="Griffens Orden")
     badge.user.add(inst_obj.hybrid)
     badge.save()
 
+#function that awards membership status whenenever the user profiles field for membership returns true
+@receiver(post_save, sender=Hybrid)
+def MemberBadge(sender=Hybrid, **kwargs):
+    inst_obj = kwargs['instance']
+    badge = Badge.objects.get(name="Medlemskaps Medalje")
+    if inst_obj.member == True:
+        badge.user.add(inst_obj)
+        badge.save()
+
+#function that awards 1,3,5 year medals based on the amount of time they have been in Hybrida, not necessarily which year they are in
+
+
+#===========================================================================================================================#
 
 #Senders
 def Send_GriffBadge():
