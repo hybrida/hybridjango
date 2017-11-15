cfc7a3b2a83ade0597635c868c8df96c31e85e6c
Sirikam <ben.dik@live.no>
2017-11-15 20:22:27 +0100

    workchanges

--- apps/achievements/apps.py
+++ apps/achievements/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
-
 class AchievementsConfig(AppConfig):
     name = 'apps.achievements'
     verbose_name = 'Achievements'
 
     def ready(self):
-        from apps.achievements.signals import handlers  # noqa
+        from apps.achievements.signals import handlers, signals
