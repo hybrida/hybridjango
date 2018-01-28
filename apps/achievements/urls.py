6a81e0787f58c512d27a88c8867f7150f3de6aa0
jonren <jonren@stud.ntnu.no>
2018-01-28 16:56:39 +0100

    Adding page /achievements/scoreboard and view for scoreboard

--- apps/achievements/urls.py
+++ apps/achievements/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-        url(r'^$', BadgeView.as_view(template_name='achievments/achievments_overview.html') ,name='achievements'),
+        url(r'^badges/$', BadgeView.as_view(template_name='achievments/achievments_overview.html'), name='achievements'),
+        url(r'^scoreboard/$', ScoreboardView.as_view(template_name='achievments/scoreboard.html'), name='scoreboard'),
 
 ]
