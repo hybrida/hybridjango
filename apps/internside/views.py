4002612f1b7d6a2ac30255e94e9971a40e1bcb55
sindreeo <sindreeo@stud.ntnu.no>
2018-02-13 19:22:51 +0100

    init

---
+++ apps/internside/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import commite_member, referat
+# Create your views here.
+def index(request):
+    commite_members = commite_member.objects.all()
+    referats = referat.objects.all().order_by('date').reverse()
+
+
+    return (request,internside.html, {"commite_members": commite_members, "referats": referats })
+
+
+def cake_list(request):
+
+    list = commite_member.objects.all().order_by('number_on_list')
+
+
