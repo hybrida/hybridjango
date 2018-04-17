577ecf4a1e2d5ccaa07a319a3eae81fc816b5a1d
Simen <simennj@outlook.com>
2018-04-17 22:04:09 +0200

    add comments and additional help text to import_csv

--- apps/registration/management/commands/import_csv.py
+++ apps/registration/management/commands/import_csv.py
@@ -1,12 +1,20 @@
 import csv
+
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from apps.registration.models import Hybrid
 
 
+# Example:
+# username,first_name,last_name,graduation_year
+# luigi,Luigi,Mario,2020
+# mario,Mario,Mario,2020
+
 class Command(BaseCommand):
-    help = 'Import hybrids from given CSV file, file needs to have a header line'
+    help = 'Import hybrids from given CSV file, file needs to have a header with username and the included fields\n' \
+           'Remember that whitespace is included so "username, first_name, last_name" is invalid,\n' \
+           'use "username,first_name,Last_name" instead (avoid whitespace around commas).\n'
 
     def add_arguments(self, parser):
         parser.add_argument('file')
