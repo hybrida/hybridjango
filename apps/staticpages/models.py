790f29631be8d4a3f1ae59b95f3ce7bbd606021c
sindreeo <sindreeo@stud.ntnu.no>
2018-04-18 13:36:04 +0200

    Adds form to apply for commite

--- apps/staticpages/models.py
+++ apps/staticpages/models.py
@@ -28,3 +28,21 @@ class Application(models.Model):
 
     def get_absolute_url(self):
         return reverse('about')
+
+
+class ComApplication(models.Model):
+    navn = models.CharField(max_length=500)
+
+    Choices = (
+        ('Vevkom', 'Vevkom'),
+        ('Bedkom', 'Bedkom'),
+        ('Arrkom', 'Arrkom'),
+        ('Jentekom', 'Jentekom'),
+        ('Kjellerkom', 'Kjellerkom'),
+        ('Redaksjonen', 'Redaksjonen')
+    )
+
+    komite = models.CharField(choices=Choices, max_length=500)
+
+    def get_absolute_url(self):
+        return reverse('about')
