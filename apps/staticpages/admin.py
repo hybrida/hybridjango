082ee85d05a8abb84a1488ad3582a117741b06d5
sindreeo <sindreeo@stud.ntnu.no>
2018-04-19 12:38:50 +0200

    Adds ComApplication to admin page

--- apps/staticpages/admin.py
+++ apps/staticpages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import BoardReport, Protocol, Application
+from .models import BoardReport, Protocol, Application, ComApplication
 
 admin.site.register(BoardReport)
 admin.site.register(Protocol)
 admin.site.register(Application)
+admin.site.register(ComApplication)
