0efc49bb5f3c8b3aed32007dc2d53a6682070edb
Henning Ødeby Karlsen <henning.karlsen@teknologiporten.no>
2018-08-28 22:06:55 +0200

    Fix formatting error in mail, also increased readability.

--- apps/events/views.py
+++ apps/events/views.py
@@ -72,11 +72,13 @@ class EventView(generic.DetailView):
 def SendAdmittedMail(hybrid, attendance):
     mail = [hybrid.email if hybrid.email else '{}@stud.ntnu.no'.format(hybrid.username)]
     successful = send_mail(
-            'Du har fått plass på {title}',
-            'Hei {name},\n\nDet er en glede å meddele at du har fått plass på {title}\n'
-            '{url}'.format(url="https://hybrida.no/hendelser/" + str(attendance.event.pk),
-            title = attendance.event.title,
-            name = hybrid.get_full_name()),
+            'Du har fått plass på {title}'
+                .format(title=attendance.event.title),
+            'Hei {name},\n\nDet er en glede å meddele at du har fått plass på {title}\n{url}'
+                .format(url="https://hybrida.no/hendelser/" + str(attendance.event.pk),
+                    title = attendance.event.title,
+                    name = hybrid.get_full_name()
+                ),
             'robot@hybrida.no',
             mail,
         )
