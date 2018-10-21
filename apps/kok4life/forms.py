d622672de5f91a4744095a7b499eda59555f02f0
DragonSlayer <eivinbh@stud.ntnu.no>
2018-10-21 16:47:45 +0200

    lagd forms

---
+++ apps/kok4life/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Subject, File
+
+class KokfForm(forms.ModelForm):
+    class meta:
+        model = File
+        fields = [
+            'navn'
+            'file'
+        ]
+
+class KoksForm(forms.ModelForm):
+    class meta:
+        model = Subject
+        fields = ['navn']
+
+
