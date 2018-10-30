0bf7273fde3a802d616cc5a4d5b8f173608ae442
Lovise Vist <lovisev@stud.ntnu.no>
2018-10-30 21:10:23 +0100

    Created Course and Evaluation models

--- apps/evaluation/models.py
+++ apps/evaluation/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+
+    name = models.CharField(max_length=255, blank=False, null=False)
+    course_code = models.CharField(max_length=255, blank=False, null=False)
+    grades_link = models.CharField(max_length=255)
+    ntnu_link = models.CharField(max_length=255)
+
+
+class Evaluation(models.Model):
+
+    author = models.ForeignKey(Hybrid, on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="evaluations")
+    lecturer = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False)
+    year = models.IntegerField(default=2010, max_length=4, blank=False, null=False)
+    semester = models.IntegerField(blank=False, null=False)
+    season = models.CharField(max_length=255, blank=False, null=False)
+    title = models.CharField(max_length=255)
+    evaluation_lecturer = models.TextField()
+    evaluation_course = models.TextField()
