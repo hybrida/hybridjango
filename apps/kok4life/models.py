cc5c88b0bc02aca50f171bba1c2ad791b61324e2
Harald <haralwit@stud.ntnu.no>
2019-02-06 17:14:22 +0100

    Add string name to models

--- apps/kok4life/models.py
+++ apps/kok4life/models.py
@@ -4,8 +4,15 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class File(models.Model):
     name = models.CharField(max_length=500)
     file = models.FileField(upload_to='kok')
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
