647dc02478eb4c220e326c8a175dd4267e6de12f
sigridhs <sigridstrandbakke@gmail.com>
2019-02-06 18:01:34 +0100

    Adds average_score field to Course model

--- apps/evaluation/models.py
+++ apps/evaluation/models.py
@@ -7,6 +7,8 @@ class Course(models.Model):
     course_code = models.CharField(max_length=255, blank=False, null=False)
     grades_link = models.CharField(max_length=255)
     ntnu_link = models.CharField(max_length=255)
+    average_score = models.DecimalField(decimal_places=2, max_digits=3,blank=True, null=True )
+
 
 
 class Evaluation(models.Model):
@@ -23,8 +25,8 @@ class Evaluation(models.Model):
         blank=False,
         null=False)
     year = models.IntegerField(default=2010, max_length=4, blank=False, null=False)
-    semester = models.IntegerField(blank=False, null=False)
-    season = models.CharField(max_length=255, blank=False, null=False)
+    semester = models.IntegerField(blank=False, null=False) #1.-5.år
+    season = models.CharField(max_length=255, blank=False, null=False) #Høst/vår
     title = models.CharField(max_length=255)
     evaluation_lecturer = models.TextField()
     evaluation_course = models.TextField()
