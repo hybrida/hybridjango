a1f0ebe18eab7e26e08e3e18c4de5c1c5c322a7f
Sondre Bakke <sondremb@stud.ntnu.no>
2019-02-13 14:06:19 +0100

    Improved update_scoreboards command

--- apps/achievements/management/commands/update_scoreboards.py
+++ apps/achievements/management/commands/update_scoreboards.py
@@ -6,142 +6,49 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from apps.registration.models import Hybrid
-from ...models import Badge
 
 
 class Command(BaseCommand):
     help = ''
 
     def handle(self, *args, **options):
-        #Creating the lists that will contain the scoreboards
-        scoreboardCurrent = []
-        scoreboardAllTime = []
+        scoreboard_current = []
+        scoreboard_all_time = []
 
-        #Function to create the Current Scoreboard
+        current_year = int(datetime.now().year)
+
         for hybrid in Hybrid.objects.all():
-
-            badgeliste = [] #List that will contain the names of the badges the user has
-
-            #Checking if a user hasn't graduated yet
-            grad_year = hybrid.graduation_year
-            current_year = int(datetime.now().year)  # Getting the current datetime
-
-            if grad_year - current_year >= 0:
-
-                score = 0
-                username = hybrid.username
-                full_name = hybrid.full_name
-
-                #Funcion that checks which badges the user has, then adding the badges the user has to the badgeliste and the score of the badges to the users score
-                for badge in Badge.objects.all():
-                    for user in badge.user.all():
-                        if username in user.username:
-                            badgeliste.append(badge.name)
-                            score += badge.scorepoints
-                #Dictionary containing all the information needed for the scoreboard
-                hybrid_dict = {
-                    'Username': username,
-                    'Score': score,
-                    'Full_Name': full_name,
-                    'Badger': badgeliste,
-                    'Number': 0,
-                }
-                #Adding the dictionary to the scoreboard list
-                scoreboardCurrent.append(hybrid_dict)
-
-
-
-        #Function to create the All Time scoreboard
-        for hybrid in Hybrid.objects.all():
-
-            badgeliste = []#List that will contain the names of the badges the user has
-            score = 0
-            username = hybrid.username
-            full_name = hybrid.full_name
-
-            # Funcion that checks which badges the user has, then adding the badges the user has to the badgeliste and the score of the badges to the users score
-            for badge in Badge.objects.all():
-                for user in badge.user.all():
-                    if username in user.username:
-                        badgeliste.append(badge.name)
-                        score += badge.scorepoints
-            #Dictionary containing all the information needed for the scoreboard
+            badges = hybrid.hybridbadges.all()
             hybrid_dict = {
-                'Username': username,
-                'Score': score,
-                'Full_Name': full_name,
-                'Badger': badgeliste,
+                'Username': hybrid.username,
+                'Score': sum(badge.scorepoints for badge in badges),
+                'Full_Name': hybrid.full_name,
+                'Badger': [badge.name for badge in badges],
                 'Number': 0,
             }
-            # Adding the dictionary to the scoreboard list
-            scoreboardAllTime.append(hybrid_dict)
 
-        #Sorting the Current scoreboard
-        switch = True
-        while (switch):
+            scoreboard_all_time.append(hybrid_dict)
 
-            switch = False
-            for i in range(len(scoreboardCurrent) - 1):
-                hybrid1 = scoreboardCurrent[i]
-                hybrid2 = scoreboardCurrent[i + 1]
+            if hybrid.graduation_year >= current_year:
+                scoreboard_current.append(hybrid_dict.copy())
 
-                if hybrid1['Score'] < hybrid2['Score']:
-                    scoreboardCurrent[i] = hybrid2
-                    scoreboardCurrent[i + 1] = hybrid1
-                    switch = True
+        scoreboard_current.sort(key=lambda dct: dct['Score'], reverse=True)
+        scoreboard_all_time.sort(key=lambda dct: dct['Score'], reverse=True)
 
-        #Adding what place a user has on the scoreboard
-        x = 0
-        y = 0
-        for i in range(len(scoreboardCurrent) - 1):
-            x += 1
+        for lst in [scoreboard_all_time, scoreboard_current]:
+            current_score = None
+            current_pos = 0
+            step = 1
+            for dct in lst:
+                if dct['Score'] == current_score:
+                    step += 1
+                else:
+                    current_score = dct['Score']
+                    current_pos += step
+                    step = 1
+                dct['Number'] = current_pos
 
-            hybrid1 = scoreboardCurrent[i]
-            hybrid2 = scoreboardCurrent[i + 1]
-
-            if hybrid1['Score'] == hybrid2['Score']:
-                hybrid1['Number'] = x-y
-                y += 1
-            elif y != 0:
-                hybrid1['Number'] = x - y
-                y = 0
-            else:
-                hybrid1['Number'] = x
-
-        #Sorting the All Time scoreboard
-        switch = True
-        while (switch):
-
-            switch = False
-            for i in range(len(scoreboardAllTime) - 1):
-                hybrid1 = scoreboardAllTime[i]
-                hybrid2 = scoreboardAllTime[i + 1]
-
-                if hybrid1['Score'] < hybrid2['Score']:
-                    scoreboardAllTime[i] = hybrid2
-                    scoreboardAllTime[i + 1] = hybrid1
-                    switch = True
-
-        # Adding what place a user has on the scoreboard
-        x = 0
-        y = 0
-        for i in range(len(scoreboardAllTime) - 1):
-            x += 1
-
-            hybrid1 = scoreboardAllTime[i]
-            hybrid2 = scoreboardAllTime[i + 1]
-
-            if hybrid1['Score'] == hybrid2['Score']:
-                hybrid1['Number'] = x - y
-                y += 1
-            elif y != 0:
-                hybrid1['Number'] = x - y
-                y = 0
-            else:
-                hybrid1['Number'] = x
-
-        #Wrighting the two scoreboards to seperate .json files in /uploads
         with open(path.join(settings.MEDIA_ROOT, "ScoreboardAllTime.json"), "w") as file:
-            json.dump(scoreboardAllTime, file)
+            json.dump(scoreboard_all_time, file)
         with open(path.join(settings.MEDIA_ROOT, "ScoreboardCurrent.json"), "w") as file:
-            json.dump(scoreboardCurrent, file)
+            json.dump(scoreboard_current, file)
