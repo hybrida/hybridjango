65b1214192b8a1f5206992c5f7c96c6fdbe42070
Sondre Bakke <sondremb@stud.ntnu.no>
2019-03-03 15:29:50 +0100

    Add functionality for automatic upload to Google Drive

--- hybridjango/management/commands/backup_db.py
+++ hybridjango/management/commands/backup_db.py
@@ -3,17 +3,27 @@ from django.core.management import call_command
 from datetime import datetime
 from os import path, mkdir
 from hybridjango import settings
+from hybridjango.utils import ISO_8601, drive
 
 
 class Command(BaseCommand):
     help = 'Creates a backup of the database'
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--upload',
+            action='store_true',
+            dest='upload',
+            help='Upload backup file to Google Drive',
+        )
 
     def handle(self, *args, **options):
         # exclude troublesome or otherwise unnecessary tables
         exclude = ['sessions', 'auth.Permission', 'contenttypes', 'admin.LogEntry']
         # filename with date as ISO-8601 (YYYY-MM-DD HH:MM) format
         now = datetime.now()
-        filename = now.strftime('BACKUP %Y-%m-%d %H:%M.json')
+        filename = now.strftime('BACKUP {}.json'.format(ISO_8601))
         # backups should be in uploads/backups
         folder = path.join(settings.MEDIA_ROOT, 'backups')
         # if folder does not exist, create it
@@ -22,3 +32,6 @@ class Command(BaseCommand):
         filename = path.join(folder, filename)
         with open(filename, 'w') as f:
             call_command('dumpdata', indent=2, use_natural_foreign_keys=True, exclude=exclude, stdout=f)
+
+        if options['upload']:
+            drive.upload_db_backup(filename)
---
+++ hybridjango/utils/drive.py
@@ -0,0 +1,104 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+from datetime import datetime
+from .secrets import FOLDER_ID  # SEE BELOW
+from . import ISO_8601
+
+# This file will throw ImportErrors on the .secrets module except when in production
+# This is by design, as the secrets-folder contains sensitive information and is ignored explicitly by Git
+
+# If you desire a local setup of drive-uploads, do the following:
+# 1. Create a folder under hybridjango/utils called secrets
+# 2. Create a file in that folder called __init__.py
+# 3. In that file, add the line FOLDER_ID = $YourGoogleDriveFolderID
+# 4. Generate a valid pickled credentials file called token.pickle
+# 5. Add token.pickle to the secrets folder
+
+# For further help with step 4, see https://developers.google.com/drive/api/v3/quickstart/python
+# Required scopes for credentials file:
+# SCOPES = ['https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/drive.file']
+
+
+def get_service():
+    # Silence logging from the google API client for level WARNING or lower
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    filename = 'hybridjango/utils/secrets/token.pickle'
+    if os.path.exists(filename):
+        with open(filename, 'rb') as token:
+            creds = pickle.load(token)
+
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+
+    if not creds or not creds.valid:
+        return None
+    else:
+        return build('drive', 'v3', credentials=creds, cache_discovery=False)
+
+
+def upload_db_backup(filename):
+    """
+    Uploads a DB backup to Google Drive
+    Is used when passing --upload to the backup_db management command
+    """
+    service = get_service()
+    if service is None:
+        print("Error with credentials. Terminating.")
+        return
+
+    file_metadata = {
+        'name': os.path.basename(filename),
+        'parents': [FOLDER_ID]
+    }
+    # no MIME-type means service will guess based on extension
+    media = MediaFileUpload(filename, mimetype=None)
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
+    print("Backup successfully uploaded with file ID {}".format(file.get('id')))
+
+
+def manage_and_delete_backups(cutoff=None, keep_amount=None):
+    """
+    Check and potentially delete backup files from drive
+    :param cutoff: optional timedelta, delete all files older than now - cutoff
+    :param keep_amount: optional int, keep at most count files, delete oldest
+    :return:
+    """
+    # TODO this function is still WIP, and is not called from any custom Management Commands
+    service = get_service()
+    if service is None:
+        print("Error with credentials. Terminating.")
+        return
+
+    query = "'{}' in parents".format(FOLDER_ID)
+    results = service.files().list(
+        q=query,
+        pageSize=10,
+        fields="nextPageToken, files(id, name)"
+    ).execute()
+    files = sorted(results.get("files", []), key=lambda f: f.get("name"), reverse=True)
+    now = datetime.now()
+    count = 0
+    for file in files:
+        filename = file.get("name")
+        date, _ = filename.split(".")
+        date = date.replace("BACKUP ", "")
+        date = datetime.strptime(date, ISO_8601)
+        delta = now - date
+        if count >= keep_amount:
+            print("{}, deleting (age {})".format(filename, delta))
+            service.files().delete(fileId=file.get("id")).execute()
+        if delta < cutoff:
+            print("{}, keeping".format(filename))
+        else:
+            print("{}, deleting (age {})".format(filename, delta))
+            service.files().delete(fileId=file.get("id")).execute()
