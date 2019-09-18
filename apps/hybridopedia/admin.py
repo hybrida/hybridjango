83e40046836a4185840e8e88a82d592f14a0c750
kristskv <kristskv@stud.ntnu.no>
2019-09-18 17:25:26 +0200

    added kok4life filer actually did it

---
+++ apps/hybridopedia/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Subject, File
+
+admin.site.register(Subject)
+admin.site.register(File)
---
+++ apps/hybridopedia/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import File, Subject
+from .forms import KokFileForm, KokSubjectForm
+
+
+# TODO: Login required for forms?
+# TODO: PEP 8?
+def fileForm(request):
+    if request.method == 'POST':
+        form = KokFileForm(request.POST, request.FILES)
+        form.instance.author = request.user
+        if form.is_valid():
+            form.save()
+            return redirect('kok:firstPage')
+    else:
+        form = KokFileForm()
+    return render(request, 'kok/fileForm.html', {
+        'form': form
+    })
+
+
+def subjectForm(request):
+    if request.method == 'POST':
+        form = KokSubjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('kok:firstPage')
+    else:
+        form = KokSubjectForm()
+        return render(request, 'kok/subjectForm.html', {'form': form})
+
+
+@login_required
+def firstPage(request):
+    subjects = Subject.objects.all()
+    return render(request, "kok/firstPage.html", {"subjects": subjects})
+
+
+@login_required
+def filePage(request, pk):
+    subject = Subject.objects.filter(pk=pk)
+    files = File.objects.filter(subject__in=subject)
+    return render(request, "kok/filePage.html", {"subject": subject, "files": files})
