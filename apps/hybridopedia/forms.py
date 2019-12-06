fbef56b7c34ced1f6c7aaf3a75441f0147013d8f
jonren <jonren@stud.ntnu.no>
2019-12-06 16:15:28 +0100

    Adjusting the subject form to match the changes made to the form in this branch after merging the move of the Subject class from hybridopedia to registration to this branch

--- apps/hybridopedia/forms.py
+++ apps/hybridopedia/forms.py
@@ -1,17 +1,7 @@
 from django import forms
-from django.core.validators import RegexValidator
 
 from hybridjango.mixins import BootstrapFormMixin
 from .models import File
-
-# This regex validator checks that the field is on the format <subject code> - <subject name>
-# Subject code is 3 or 4 capital letters, then 4 digits
-# Subject name is any number of words with any
-subject_name_validator = RegexValidator(
-    r'^[A-Z]{3,4}[0-9]{4}\s-([\s][\wæøåÆØÅ]+)+$',
-    "Name is not on the correct format")
-
-#name = forms.CharField(validators=[subject_name_validator], required=True)
 
 
 class HybridopediaFileForm(forms.ModelForm, BootstrapFormMixin):
--- apps/registration/forms.py
+++ apps/registration/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 from hybridjango.mixins import BootstrapFormMixin
 from .models import Hybrid, Subject
+
+
+# This regex validator checks that the field is on the format <subject code> - <subject name>
+# Subject code is 3 or 4 capital letters, then 4 digits
+# Subject name is any number of words with any
+subject_name_validator = RegexValidator(
+    r'^[A-Z]{3,4}[0-9]{4}\s-([\s][\wæøåÆØÅ]+)+$',
+    "Name is not on the correct format")
 
 
 class HybridForm(forms.ModelForm):
@@ -28,6 +37,4 @@ class SubjectForm(forms.ModelForm, BootstrapFormMixin):
         exclude = ['author']
         fields = ['name']
 
-    def __init__(self, *args, **kwargs):
-        super(SubjectForm, self).__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+    name = forms.CharField(validators=[subject_name_validator], required=True)
