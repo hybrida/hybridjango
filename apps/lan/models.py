8bd0e00bf21d7af6439c286553d7eaa6f08a7a19
Lars Skifjeld <larsski@ntnu.no>
2020-02-12 18:06:05 +0100

    Add bracket match and team models

--- apps/lan/models.py
+++ apps/lan/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from apps.registration.models import Hybrid
 
-# Create your models here.
+
+class Team(models.Model):
+    """
+    Uncomment when comp is added
+    class Meta:
+        unique_together=('name', 'comp')
+    """
+    users = models.ManyToManyField(Hybrid, blank=False)
+
+    def default_name(self):
+        return self.users.first().full_name
+
+    name = models.CharField(
+        max_length=255,
+        default=default_name
+    )
+
+
+class Bracket(models.Model):
+    # Add foreignkey to comp
+    pass
+
+
+class Match(models.Model):
+    teams = models.ManyToManyField(Team)
+    next_match = models.ForeignKey('Match', related_name='prev_match', on_delete=models.SET_NULL)
+    bracket = models.ForeignKey(Bracket, related_name='matches')
