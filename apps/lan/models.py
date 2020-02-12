c5999b56459e60c751c6d0aa9601995cb7762d23
JHailemariam <jonathsh@stud.ntnu.no>
2020-02-12 17:48:11 +0100

    Entry and scoreboard model in lan

--- apps/lan/models.py
+++ apps/lan/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from apps.registration.models import Hybrid
+
+
+class Scoreboard(models.Model):
+    pass
+
+
+class Entry(models.Model):
+    user = models.ForeignKey(Hybrid, null=False, blank=False)
+    value = models.DoubleField(null=False, blank=False)
+    scoreboard = models.ForeignKey(Scoreboard, related_name='entries', blank=False)
+
+
